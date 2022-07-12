2e77f7d109cd5896e1d861029c84743252b81d5b
JerryTian26153 <95434328+JerryTian26153@users.noreply.github.com>
2022-07-13 00:17:22 +0800

    0.5

--- js/script.js
+++ js/script.js
@@ -39,6 +39,17 @@
             });//取得所有待審核預約json完成
 
         });
+        var votingSelectionTable = $("#votingSelectionTable")
+        var votingSelectionTemplate =
+            ' <div class="input-group mb-1">\n' +
+            '   <span class="input-group-text">選項名</span>\n' +
+            '   <input type="text" class="form-control voteName_input">\n' +
+            '   <span class="input-group-text">關鍵字</span>\n' +
+            '   <input type="text" class="form-control voteName_input">\n' +
+            ' </div>'
+        $("#addSelectionButton").click(function (e) {
+            votingSelectionTable.append(votingSelectionTemplate);
+        });
 
     })
 
