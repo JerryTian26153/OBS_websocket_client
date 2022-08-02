8351f72d8ac8e787eb8c52339c41c455ef8de23a
JerryTian26153 <95434328+JerryTian26153@users.noreply.github.com>
2022-08-03 00:48:36 +0800

    更新
    投票增加ID

--- js/script_vote.js
+++ js/script_vote.js
@@ -2,17 +2,19 @@
 {
     //jquery
     $(document).ready(function (){
-
+        var i = 2;
         var votingSelectionTable = $("#votingSelectionTable")
-        var votingSelectionTemplate =
-            ' <div class="input-group mb-1">\n' +
-            '   <span class="input-group-text">選項名</span>\n' +
-            '   <input type="text" class="form-control voteName_input">\n' +
-            '   <span class="input-group-text">關鍵字</span>\n' +
-            '   <input type="text" class="form-control voteName_input">\n' +
-            ' </div>'
+
         $("#addSelectionButton").click(function (e) {
+            let votingSelectionTemplate =
+                ' <div class="input-group mb-1">\n' +
+                '   <span class="input-group-text">選項名</span>\n' +
+                '   <input id="Name'+ i +'" type="text" class="form-control voteName_input">\n' +
+                '   <span class="input-group-text">關鍵字</span>\n' +
+                '   <input id="Key'+ i +'" type="text" class="form-control voteName_input">\n' +
+                ' </div>'
             votingSelectionTable.append(votingSelectionTemplate);
+            i=i+1;
         });
 
     })
