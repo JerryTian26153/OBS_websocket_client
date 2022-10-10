72ed4895d8718c1ebc8007d32a1e36b8b0c405e6
JerryTian26153 <95434328+JerryTian26153@users.noreply.github.com>
2022-10-10 22:36:44 +0800

    管理，直播遷入
    chatPage新增聊天室管理按鈕與直播遷入

--- js/script_chat.js
+++ js/script_chat.js
@@ -1,9 +1,28 @@
 // 此為回到最上層按鈕的JS
 {
-    edit_page = document.getElementById("chat_manager")
-    document.getElementsByClassName("edit_button").onclick = function(e) {
-        var positionX = e.clientX + 'px';
-        var positionY = e.clientX + 'px';
-        el.style.top = position;
-    };
+
+    function deleteComment() {
+
+    }
+    $(document).ready(function () {
+        $('html').click(function (e) {
+            $('.chat-manager-list').css("display","none");
+        });
+        //edit_page = document.getElementById("chat_manager")
+        $('.edit_button').click(function (e) {
+            console.log(e.target.id);
+            let elm = $(this);
+            // var xPos = e.pageX - elm.offset().left;
+            // var yPos = e.pageY - elm.offset().top;
+            let xPos = e.pageX;
+            let yPos = e.pageY;
+            console.log(xPos, yPos);
+            let count = setInterval(function() {
+                $('.chat-manager-list').css("left", elm.offset().left - 100);
+                $('.chat-manager-list').css("top", elm.offset().top + 35);
+                $('.chat-manager-list').css("display", "");
+                clearTimeout(count);
+            }, 10);
+        });
+    });
 }
--- js/script_index.js
+++ js/script_index.js
@@ -5,23 +5,6 @@
         voteResultPage = window.open('votePage.html', 'open', 'height=500, width=700');
         //voteResultPage = window.open('index.html', 'open', 'height=500, width=500');
     }
-    var chart = c3.generate({
-        bindto: "#chart",
-        data: {
-            columns: [
-                ["data1", 30, 200, 100, 400, 150, 250],
-                ["data2", 50, 20, 10, 40, 15, 25],
-            ],
-            axes: {
-                data2: "y2", // ADD
-            },
-        },
-        axis: {
-            y2: {
-                show: true, // ADD
-            },
-        },
-    });
     //jquery
     $(document).ready(function (){
         $(".page-button").click(function (e) {
