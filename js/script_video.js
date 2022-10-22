d41a5511cab7ce305061fc3ba9638d346613f125
JerryTian26153 <95434328+JerryTian26153@users.noreply.github.com>
2022-10-23 03:36:42 +0800

    update_ver0.5
    此為測試merge版本

--- js/script_video.js
+++ js/script_video.js
@@ -1,52 +1,16 @@
 // 此為回到最上層按鈕的JS
 {
-
-    const videoCardContainer = document.querySelector('.video-container');
-
-    let api_key = "AIzaSyBLywrpuktG65bWffVToT3co9rB7DNZoBE"
-    let video_http = "https://www.googleapis.com/youtube/v3/videos?";
-    let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
-
-    fetch(video_http + new URLSearchParams({
-        key:api_key,
-        part: 'snippet',
-        chart: 'mostPopular',
-        maxResults: 50,
-        regionCode: 'tw'
-    }))
-    .then(res => res.json())
-    .then(data =>{
-        // console.log(data);
-        data.items.forEach(item => {
-            getChannelIcon(item);
-        })
-    })
-    .catch(err => console.log(err));
-
-    const getChannelIcon = (video_data) => {
-        fetch(channel_http + new URLSearchParams({
-            key: api_key,
-            part: 'snippet',
-            id: video_data.snippet.channelId
-        }))
-            .then(res => res.json())
-            .then(data =>{
-                video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-                makeVideoCard(video_data);
-            })
+    const video_focus = document.querySelector('.video-focus');
+    function video_details_show(target) {
+        let page = target;
+        console.log(page);
+        video_focus.innerHTML=page;
     }
-    const  makeVideoCard = (data) => {
-        videoCardContainer.innerHTML +=`
-            <div class="video" onclick="location.href ='https://youtube.com/watch?v=${data.id}'">
-                <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
-                    <div class="content">
-                        <img src="${data.channelThumbnail}" class="channel-icon" alt="">
-                        <div class="info">
-                            <h4 class="title">${data.snippet.title}</h4>
-                            <p class="channel-name">${data.snippet.channelTitle}</p>
-                        </div>
-                    </div>
-            </div>
-        `
-    }
+//jquery
+    /*$(document).ready(function (){
+        $(".video").click(function (e) {
+            console.log(e.target);
+            //$("." + e.target.id).css("display","")
+        });
+    })*/
 }
